2654667be6245ccf61ebc6405c65cbd6ba82c3ab
123Kevin4567 <kevinpolodr@gmail.com>
2025-02-15 15:45:17 -0500

    Dom

---
+++ app.js
@@ -0,0 +1,29 @@
+
+const header = dom.querySelector('header');
+const paragraph = dom.querySelector('p');
+let styles = dom.styleSheets;
+const script = dom.scripts;
+const links = dom.links;
+const img = dom.images;
+const form = dom.forms;
+const documento = dom.getElementByid('documento');
+const api = dom.getElementByid("api");
+const listaA = dom.getElementsByClassName('listaA');
+const listaB = dom.getElementsByClassName('listaB');
+
+const hermanos= dom.querySelectorAll('ul.list > li.item');
+
+console.log(padre.childNodes);
+console.log(padre.childElementCount);
+console.log(padre.children);
+
+
+
+
+// console.log(form);
+// console.log(document);
+// console.log(api);
+// console.log(lista);
+// console.log(listaA.lenght);
+// console.log(listaB.lenght);
+
